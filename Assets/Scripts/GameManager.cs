3f5f473355c35cd377eeac807d12eb808ecc7d15
Guy Yomtov <guyyomtov13@gmail.com>
2023-08-13 21:21:07 +0300

    create game manager and animated script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public float gameSpeed { get; private set; }
+    public float initialGameSpeed = 5f;
+    public float gameSoeedIncrease = 0.1f;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        gameSpeed = initialGameSpeed;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Debug.Log($"game speed: {gameSpeed}");
+        gameSpeed += gameSoeedIncrease * Time.deltaTime;
+    }
+}
