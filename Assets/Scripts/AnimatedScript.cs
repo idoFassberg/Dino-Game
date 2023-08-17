e4df670fc0309af6baadc163cb2b927d066c4ae9
Guy Yomtov <guyyomtov13@gmail.com>
2023-08-17 20:35:44 +0300

    using update instead invoke in animated script

--- Assets/Scripts/AnimatedScript.cs
+++ Assets/Scripts/AnimatedScript.cs
@@ -7,26 +7,49 @@ public class AnimatedScript : MonoBehaviour
     public Sprite[] sprites;
     [SerializeField] private SpriteRenderer spriteRenderer;
     private int frame;
+    private float nextCallTime = 0f;
+    private float interval = 0f; 
 
-    private void OnEnable()
+    // private void OnEnable()
+    // {
+    //     Invoke(nameof(Animate), 0f);
+    // }
+    // private void OnDisable()
+    // {
+    //     CancelInvoke();   
+    // }
+    // private void Animate()
+    // {
+    //     ++frame;
+    //     if(frame >= sprites.Length)
+    //     {
+    //         frame = 0;
+    //     }
+    //     if(frame >= 0 && frame < sprites.Length)
+    //     {
+    //         spriteRenderer.sprite = sprites[frame];
+    //     }
+    //     Invoke(nameof(Animate), 1f / GameManager.Instance.gameSpeed);
+    // }
+    public void Start()
     {
-        Invoke(nameof(Animate), 0f);
+        nextCallTime = Time.time + interval;
     }
-    private void OnDisable()
+    public void Update()
     {
-        CancelInvoke();   
-    }
-    private void Animate()
-    {
-        ++frame;
-        if(frame >= sprites.Length)
+        if(Time.time >= nextCallTime)
         {
-            frame = 0;
+            ++frame;
+            if(frame >= sprites.Length)
+            {
+                frame = 0;
+            }
+            if(frame >= 0 && frame < sprites.Length)
+            {
+                spriteRenderer.sprite = sprites[frame];
+            }
+            nextCallTime = Time.time + 1f / GameManager.Instance.gameSpeed;
         }
-        if(frame >= 0 && frame < sprites.Length)
-        {
-            spriteRenderer.sprite = sprites[frame];
-        }
-        Invoke(nameof(Animate), 1f / GameManager.Instance.gameSpeed);
+         
     }
 }
