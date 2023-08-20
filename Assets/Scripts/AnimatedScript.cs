afb6c6e0657c913a8d5d713ad65b078838d3a1c5
Guy Yomtov <guyyomtov13@gmail.com>
2023-08-20 20:43:53 +0300

    create main menu scene

--- Assets/Scripts/AnimatedScript.cs
+++ Assets/Scripts/AnimatedScript.cs
@@ -1,55 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class AnimatedScript : MonoBehaviour
 {
     public Sprite[] sprites;
     [SerializeField] private SpriteRenderer spriteRenderer;
-    private int frame;
+    private int frame = 0;
     private float nextCallTime = 0f;
-    private float interval = 0f; 
-
-    // private void OnEnable()
-    // {
-    //     Invoke(nameof(Animate), 0f);
-    // }
-    // private void OnDisable()
-    // {
-    //     CancelInvoke();   
-    // }
-    // private void Animate()
-    // {
-    //     ++frame;
-    //     if(frame >= sprites.Length)
-    //     {
-    //         frame = 0;
-    //     }
-    //     if(frame >= 0 && frame < sprites.Length)
-    //     {
-    //         spriteRenderer.sprite = sprites[frame];
-    //     }
-    //     Invoke(nameof(Animate), 1f / GameManager.Instance.gameSpeed);
-    // }
+    private float interval = 0f;
+    public bool render = false;
+    
     public void Start()
-    {
-        nextCallTime = Time.time + interval;
-    }
-    public void Update()
-    {
-        if(Time.time >= nextCallTime)
-        {
-            ++frame;
-            if(frame >= sprites.Length)
-            {
-                frame = 0;
-            }
-            if(frame >= 0 && frame < sprites.Length)
-            {
-                spriteRenderer.sprite = sprites[frame];
-            }
-            nextCallTime = Time.time + 1f / GameManager.Instance.gameSpeed;
-        }
-         
-    }
+     {
+         nextCallTime = Time.time + interval;
+     }
+     public void Update()
+     {
+         if(render && Time.time >= nextCallTime)
+         {
+             ++frame;
+             if(frame >= sprites.Length)
+             {
+                 frame = 0;
+             }
+             if(frame >= 0 && frame < sprites.Length)
+             {
+                 spriteRenderer.sprite = sprites[frame];
+             }
+             nextCallTime = Time.time + (1f / GameManager.Instance.gameSpeed);
+         }
+         else
+         {
+             if (!render)
+             {
+                 frame = 0;
+             }
+         }
+          
+     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,12 +12,15 @@ public class Player : MonoBehaviour
 
     [SerializeField] private Vector3 vector3;
     [SerializeField] private float moveDino;
+    [SerializeField] private AnimatedScript animatedScriptRun;
+    [SerializeField] private AnimatedScript animatedScriptJump;
     
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            animatedScriptJump.render = true;
+            animatedScriptRun.render = false;
             verticalVelocity = jumpForce;
         } 
         
@@ -25,10 +28,12 @@ public class Player : MonoBehaviour
         verticalVelocity += gravity * Time.deltaTime;
         
         // Update the dino's vertical position
-        dinoTransform.position += new Vector3(0f, verticalVelocity * Time.deltaTime, 0f);
+        dinoTransform.position += new Vector3(0f, verticalVelocity * 3 * Time.deltaTime, 0f);
 
         if (dinoTransform.position.y <= -1.66f)
         {
+            animatedScriptJump.render = false;
+            animatedScriptRun.render = true;
             dinoTransform.position = new Vector3(dinoTransform.position.x, -1.66f, dinoTransform.position.z);
             verticalVelocity = 0f;
         }
